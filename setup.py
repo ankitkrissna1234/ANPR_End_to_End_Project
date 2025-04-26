0749e585551a616d67e66799206d1b7153173292
ankitkrissna1234 <ankitkusahu7846@gmail.com>
2025-04-26 11:33:39 +0530

    requrements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="anpr",
+    version="0.0.0",
+    author="Ankit7846",
+    author_email="sahuankitkumar205@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
